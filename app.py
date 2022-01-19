a80a2485d97ceb7592de563a3c778c75f9860d29
Clement Prolifel Priyatama <clementprolifel@gmail.com>
2022-01-19 15:52:48 +0700

    add find transaction volume

--- app.py
+++ app.py
@@ -21,13 +21,10 @@ authData = json.dumps({
 c = Client(api_key_id=KEY_ID, api_key_secret=KEY_SECRET)
 client_id = c.get_balances('IDR')['balance'][0]['account_id']
 
-# Check IDR balance
-# If IDR > 700k, return true
-# else return false
-def checkBalance(client):
+def checkBalance(client, currency):
     try:
-        result = client.get_balances('IDR')['balance'][0]['balance']
-        return float(result) if float(result) > 700000 else False
+        result = client.get_balances(currency)['balance'][0]['balance']
+        return float(result)
     except Exception as e:
         print(e)
         return False
@@ -58,38 +55,44 @@ def getTopOrderBook(client, step):
 
 # Get triangular arbitrage percentage based on top 1 order book
 def getPercentage(first, second, third):
-    percentage = ((float(third[0]['price']) / float(second[0]['price'])) / float(first[0]['price'])) * 100
+    percentage = ((float(third['price']) / float(second['price'])) / float(first['price'])) * 100
     return percentage
 
 def getBestPrice(first, second, third):
     cleanPrices = {}
-    cleanPrices['1'] = float(first[0]['price']) * float(first[0]['volume'])
-    cleanPrices['2'] = float(second[0]['price']) * float(second[0]['volume']) * float(first[0]['price'])
-    cleanPrices['3'] = float(third[0]['price']) * float(third[0]['volume'])
+    cleanPrices['1'] = float(first['price']) * float(first['volume'])
+    cleanPrices['2'] = float(second['price']) * float(second['volume']) * float(first['price'])
+    cleanPrices['3'] = float(third['price']) * float(third['volume'])
     
-    lowestPrice, bestPrice = min(cleanPrices.items(), key=lambda x:x[1])
-    print(f'bestprice is {lowestPrice} with volume {bestPrice}')
+    lowestPair, bestPrice = min(cleanPrices.items(), key=lambda x:x[1])
+    print(f'lowestPair is {lowestPair} with price {bestPrice}')
     
-    return lowestPrice
-    
-    # volume1 = bestPrice / float(first[0]['price'])
-    # volume2 = bestPrice / (float(second[0]['price']) * float(third[0]['price']))
-    # volume3 = bestPrice / float(third[0]['price'])
-    
-    # return "{:.6f}".format(volume1), "{:.6f}".format(volume2), "{:.6f}".format(volume3)
-    
-def getTransactionVolume(lowestPrice):
-    print(type(lowestPrice))
-    
+    return float(bestPrice)
 
+    
+def getTransactionVolume(client, bestPrice, first, second):
+    balanceIDR = checkBalance(client, 'IDR')
+    if balanceIDR >= bestPrice:
+        volume1 = bestPrice / float(first["price"])
+    else:
+        volume1 = balanceIDR / float(first["price"])
+    
+    # volume1 = balanceIDR / float(first["price"])
+    volume2 = 99.8 / 100 * volume1 / float(second["price"])
+    volume3 = 99.9 / 100 * volume2
+    
+    # kalau volume > 0.00015, execute
+    
+    return "{:.6f}".format(volume1), "{:.6f}".format(volume2), "{:.6f}".format(volume3)
+    
 # Create market order
 def createOrder(client, price1, volume1, price2, volume2, price3, volume3):
     try:
-        client.post_limit_order('XBTIDR', price1, 'ASK', volume1)
+        client.post_limit_order('XBTIDR', price1, 'ASK', volume1, post_only=True)
         try:
-            client.post_limit_order('ETHXBT', price2, 'ASK', volume2)
+            client.post_limit_order('ETHXBT', price2, 'ASK', volume2, post_only=True)
             try:
-                client.post_limit_order('ETHIDR', price3, 'BID', volume3)
+                client.post_limit_order('ETHIDR', price3, 'BID', volume3, post_only=True)
             except Exception as e:
                 print('error 3rd: ', e)
                 return
@@ -103,30 +106,26 @@ def createOrder(client, price1, volume1, price2, volume2, price3, volume3):
     print('triagular arbitrage done')
 
 while True:
-    first = getTopOrderBook(c, 1)
-    second = getTopOrderBook(c, 2)
-    third = getTopOrderBook(c, 3)
+    first = getTopOrderBook(c, 1)[0]
+    second = getTopOrderBook(c, 2)[0]
+    third = getTopOrderBook(c, 3)[0]
     
-    print(f'price1: {first[0]["price"]}. volume1: {first[0]["volume"]}')
-    print(f'price2: {second[0]["price"]}. volume2: {second[0]["volume"]}')
-    print(f'price3: {third[0]["price"]}. volume3: {third[0]["volume"]}')
+    # print(f'price1: {first["price"]}. volume1: {first["volume"]}')
+    # print(f'price2: {second["price"]}. volume2: {second["volume"]}')
+    # print(f'price3: {third["price"]}. volume3: {third["volume"]}')
     
-    percentage = getBestPrice(first, second, third)
+    percentage = getPercentage(first, second, third)
     print(f'percentage is {percentage}')
     
     if percentage > 100.5:
-        price1 = first[0]['price']
-        price2 = second[0]['price']
-        price3 = third[0]['price']
-        volume1, volume2, volume3 = getVolume(first, second, third)
-        print(price1, price2, price3)
-        print(type(price1), type(price2), type(price3))
-        print(volume1, volume2, volume3)
-        print(type(volume1), type(volume2), type(volume3))
-        # createOrder(c, price1, volume1, price2, volume2, price3, volume3)
+        price1 = first['price']
+        price2 = second['price']
+        price3 = third['price']
+        volume1, volume2, volume3 = getTransactionVolume(c, getBestPrice(first, second, third), first, second)
+        
+        # print(price1, price2, price3)
+        # print(type(price1), type(price2), type(price3))
+        # print(volume1, volume2, volume3)
+        # print(type(volume1), type(volume2), type(volume3))
+        createOrder(c, price1, volume1, price2, volume2, price3, volume3)
         break
-
-# first = getTopOrderBook(c, 1)
-# second = getTopOrderBook(c, 2)
-# third = getTopOrderBook(c, 3)
-# print(c.get_balances('IDR')['balance'][0]['account_id'])
